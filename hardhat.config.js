13482b9c2d8acadfbdb5efffb28175e15da37915
Amarswain450 <amarswain450@gmail.com>
2024-02-02 16:58:34 +0530

    final code in blockchain chai project

---
+++ hardhat.config.js
@@ -0,0 +1,15 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env"});
+
+/** @type import('hardhat/config').HardhatUserConfig */
+const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
+const POLYGON_PRIVATE_KEY = process.env.POLYGON_PRIVATE_KEY;
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    mumbai: {
+      url: ALCHEMY_API_KEY_URL,
+      accounts: [POLYGON_PRIVATE_KEY],
+    }
+  },
+};
